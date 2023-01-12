a05de5440e10bf7a1bba4f8c7a6e6464153febd4
KajcsaRenataGabriela <67558003+KajcsaRenataGabriela@users.noreply.github.com>
2023-01-12 23:00:39 +0200

    Added stop sign

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,47 @@
 export default {
   data() {
     return {
-      duration: 15 * 1000,
-      elapsed: 0
-    }
+      duration: 5 * 1000,
+      elapsed: 0,
+      stopped: false
+    };
   },
   created() {
-    let lastTime = performance.now()
+    this.stopped = false;
+    let lastTime = performance.now();
     const update = () => {
-      const time = performance.now()
-      this.elapsed += Math.min(time - lastTime, this.duration - this.elapsed)
-      lastTime = time
-      this.handle = requestAnimationFrame(update)
-    }
-    update()
+      const time = performance.now();
+      this.elapsed += Math.min(time - lastTime, this.duration - this.elapsed);
+      lastTime = time;
+      this.handle = requestAnimationFrame(update);
+    };
+    update();
+    
   },
   unmounted() {
-    cancelAnimationFrame(this.handle)
-  }
-}
+    cancelAnimationFrame(this.handle);
+    this.stopped = true;
+  },
+};
 </script>
 
 <template>
-  <label>Elapsed Time: 
+  <h1 style="padding-bottom: 50px">Elapsed Time Project</h1>
+  <div :style= "elapsed / duration!=1 ? 'display: none' : ''">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/Stop_sign.png" width=200 style="padding-bottom: 50px">
+  </div>
+  <label
+    >Elapsed Time:
     <progress :value="elapsed / duration"></progress>
   </label>
-  <br>
+  <br />
   <div>{{ (elapsed / 1000).toFixed(1) }} seconds</div>
-  <br>
+  <br />
   <div>
-    Decide duration: <input type="range" v-model="duration" min="1" max="35000">
+    Decide duration:
+    <input type="range" v-model="duration" min="1" max="35000" />
     {{ (duration / 1000).toFixed(1) }} seconds
   </div>
-  <br>
+  <br />
   <button @click="elapsed = 0">Reset elapsed time</button>
 </template>
